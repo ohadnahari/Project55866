350c9b96c489e5fedbf5e85953af1528dfa65096
ohadnahari <95749044+ohadnahari@users.noreply.github.com>
2024-06-17 16:27:07 +0300

    added splitting data functions to data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split  # Import train_test_split function
+
 origin_df = pd.read_csv('customers_copy.csv')
 
 df = origin_df.copy()
@@ -19,3 +21,15 @@ df.to_csv('encoded_data.csv', index=False)
 print(genders_dictionary)
 print(cardTypes_dictionary)
 print(locations_dictionary)
+
+
+# split dataset in features and target variable
+def split_data(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1) # 70% training and 30% test
+    return x_train, x_test, y_train, y_test
+
+def feature_selection(df, predictor_list):
+    feature_cols = predictor_list
+    x = df[feature_cols]  # Features
+    y = df.win_category  # Target variable
+    return x, y
