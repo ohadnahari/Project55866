a74be367e54e533574a382d6df56267daf2f32dc
ohadnahari <95749044+ohadnahari@users.noreply.github.com>
2024-06-25 18:35:09 +0300

    Answered question 3 (regression with evaluation metrics)

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,14 +2,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split  # Import train_test_split function
 import numpy as np
 import statsmodels.api as sm
-from sklearn import datasets
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+import statsmodels.formula.api as smf
+from scipy.stats.stats import pearsonr
+
 
 origin_df = pd.read_csv('customers_copy.csv')
 
 df = origin_df.copy()
 
+# assign integer values to non-numerical values using enumerate
 unique_locations = df['Location'].unique()
 locations_dictionary = {item: index for index, item in enumerate(unique_locations)}
 unique_cardTypes = df['CardType'].unique()
@@ -17,21 +23,32 @@ cardTypes_dictionary = {item: index for index, item in enumerate(unique_cardType
 unique_genders = df['Gender'].unique()
 genders_dictionary = {item: index for index, item, in enumerate(unique_genders)}
 
+# encode the non-numeric values with the integer values assigned
 df['Location'] = df['Location'].map(locations_dictionary)
 df['CardType'] = df['CardType'].map(cardTypes_dictionary)
 df['Gender'] = df['Gender'].map(genders_dictionary)
 
+rows_to_delete = []
+for index, row in df.iterrows():
+    if row['AnnualSpending'] < 0:
+        rows_to_delete.append(index)
+
+df.drop(index=rows_to_delete, inplace=True)
+
+
+# save the encoded data
 df.to_csv('encoded_data.csv', index=False)
-
-print(genders_dictionary)
-print(cardTypes_dictionary)
-print(locations_dictionary)
-
 
 # split dataset in features and target variable
 def split_data(x, y):
+    """
+    Splits data into train and test sets
+    """
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1) # 70% training and 30% test
     return x_train, x_test, y_train, y_test
+
+
+
 
 
 def feature_selection(df, predictor_list):
@@ -76,10 +93,54 @@ def plot_regression_results(model, x, y):
 
 
 def main():
-    feature_cols = ['IncomeEstimate', 'Gender', 'Location']
-    x, y = feature_selection(df, feature_cols)
-    reg = build_regression(x, y)
-    plot_regression_results(reg, x, y)
+    training_set = pd.read_csv('encoded_data.csv')
+    training_set = training_set.drop(columns=["Unnamed: 0", "LastName", "RecordNumber", "CustomerId"])
+    # print(training_set.columns)
+    X = training_set.drop(columns=["AnnualSpending"])
+    y = training_set["AnnualSpending"]
+
+    x_train, x_test, y_train, y_test = split_data(X, y)
+    lm = LinearRegression().fit(x_train, y_train)
+    y_pred = lm.predict(x_test)
+    # print(f"the mean absolute error is: {mean_absolute_error(y_test, y_pred)}")
+    # print(f"the mean squared error is: {mean_squared_error(y_test, y_pred)}")
+    formula = 'AnnualSpending ~ ' + '+'.join(X.columns)
+    smf_model = smf.ols(formula=formula, data=training_set)
+    results = smf_model.fit()
+    # print(formula)
+    # print(results.summary())
+
+    training_set_fraction = 0.6
+    msk = np.random.rand(len(training_set)) < training_set_fraction
+    data_train = training_set[msk]
+    data_test = training_set[~msk]
+    # print('Training set size: {0:d}\nTest set size: {1:d}'.format(len(data_train), len(data_test)))
+
+    y_predicted = results.predict(data_test)
+    plt.scatter(data_test['AnnualSpending'], y_predicted)
+    plt.xlabel('real data'), plt.ylabel('predicted data')
+
+    # Adding the line y=x
+    lims = [np.min([data_test['AnnualSpending'].min(), y_predicted.min()]),
+            np.max([data_test['AnnualSpending'].max(), y_predicted.max()])]
+    plt.plot(lims, lims, 'r--', alpha=0.75, label='y = x')
+
+    # Adding the regression line
+    # Calculating the regression line
+    regression_slope, regression_intercept = np.polyfit(data_test['AnnualSpending'], y_predicted, 1)
+    regression_line = regression_slope * np.array(lims) + regression_intercept
+    plt.plot(lims, regression_line, 'b-', alpha=0.75, label='Regression Line')
+
+    # Adding a legend
+    plt.legend()
+
+    # Showing the plot
+    plt.show()
+
+    # feature_cols = ['IncomeEstimate', 'Gender', 'Location']
+    # x, y = feature_selection(df, feature_cols)
+    # reg = build_regression(x, y)
+    # plot_regression_results(reg, x, y)
 
 
 if __name__ == '__main__':
